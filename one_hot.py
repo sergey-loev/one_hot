2b568c4aa8c8d659eb978a64e9b7dede8706043f
Sergey Loev <srrgloi.994@gmail.com>
2023-11-18 01:06:53 +0300

    done

--- one_hot.py
+++ one_hot.py
@@ -1,1 +1,14 @@
-1
+import random
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
+
+columns_names = data['whoAmI'].unique()
+one_hot_data = pd.DataFrame()
+for name in columns_names:
+    one_hot_data[name] = (data['whoAmI'] == name).astype(int)    
+one_hot_data.head()
